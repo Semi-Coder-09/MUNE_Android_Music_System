1b933482deba63ec9e5caa6a0b6fe9e10fa626ab
Bhabani Sankar Mishra <71137289+Semi-Coder-09@users.noreply.github.com>
2023-07-16 15:03:11 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/mune/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mune;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
